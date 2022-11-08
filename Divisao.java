b84dbc4795c46231bf4809031479eb4e4c0d3ea3
CamilaRCM <101403356+CamilaRCM@users.noreply.github.com>
2022-11-08 19:50:49 -0300

    Add files via upload

---
+++ Divisao.java
@@ -0,0 +1,6 @@
+public class Divisao {
+        public double calcula(int numA, int numB) {
+            return (numA / numB);
+        }
+    }
+
---
+++ Exemplo.java
@@ -0,0 +1,18 @@
+public class Exemplo {
+        public static void main(String[] args) {
+            double dividendo = Double.parseDouble("10");
+            double divisor = Double.parseDouble("3");
+
+            try {
+                divide(dividendo, divisor);
+            } catch (ArithmeticException e) {
+                System.out.println("Não pode dividir por zero!");
+            }
+        }
+
+        public static void divide(double dividendo, double divisor) {
+            System.out.println("Divisão = " + (dividendo / divisor));
+        }
+    }
+
+
